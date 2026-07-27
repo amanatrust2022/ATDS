f302b7716f0ee007c7b7b9343a142f7bda89f93a
ATDS <amanatrust2022@gmail.com>
2026-07-27 12:15:25 +0100

    deleted v2 of receptioPage

--- amana-diagnostics/app/reception/page.tsx
+++ amana-diagnostics/app/reception/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-import ReceptionPageV2 from '@/components/ReceptionPage.v2';
+import ReceptionPage from '@/components/ReceptionPage';
 
 export default function LegacyReceptionPage() {
-  return <ReceptionPageV2 />;
+  return <ReceptionPage />;
 }
